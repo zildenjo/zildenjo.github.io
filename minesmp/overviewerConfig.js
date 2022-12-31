de27a0a4c26076efa16829cc0067dd49f6517567
Zildjian California <zildenjo@gmail.com>
2022-12-31 20:20:14 +0800

    Updated Overworld Viewer

---
+++ minesmp/overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "minesmp - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1672483802",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Mine-SMP Daytime",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "minesmpday",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "minesmp - overworld",
+            "last_rendertime": 1672482167,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1967,
+                181,
+                -1480
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -1967,
+                181,
+                -1480
+            ],
+            "north_direction": 0
+        }
+    ]
+};
